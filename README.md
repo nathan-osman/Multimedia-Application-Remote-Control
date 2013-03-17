ddda60ff1aa218d073b7e1047b6b7286f3f8f775
Nathan Osman <admin@quickmediasolutions.com>
2013-03-17 16:05:58 -0700

    Added better error handling to the MediaPlayer class.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ These Python scripts provide a web-based interface for controlling common Linux 
 You will need the following installed on your system in order to run PAD:
 
  - one of the supported media players
- - Python 2.x (tested on Python 2.7.3)
+ - Python 2.7.x (tested on Python 2.7.3)
  - [dbus-python](http://dbus.freedesktop.org/releases/dbus-python/) (often found in package managers under the name `python-dbus`)
 
 ### Starting MARC
@@ -21,3 +21,29 @@ Simply open a terminal and run the following command:
     python marc.py
 
 Then point your browser to [http://localhost:8000](http://localhost:8000) and enjoy!
+
+### JSON API
+
+MARC provides a JSON API that is used by the web interface to control the music players. The functions exposed are as follows:
+
+ - <code>/players</code> - a list of all media players available
+ - <code>/&lt;player&gt;/&lt;action&gt;</code> - sends one of the following actions to the specified player:
+   - <code>play</code> - begins playing the current song in the playlist
+   - <code>pause</code> - ceases playback
+   - <code>stop</code> - ceases playback and resets the play position in the current song
+   - <code>previous</code> - begins playing the previous song in the playlist
+   - <code>next</code> - begins playing the next song in the playlist
+
+The JSON data returned always consists of information relating to the currently playing song (if any). For example:
+
+    {
+        'song': {
+            'title': 'Song Title',
+            'artist': 'Song Artist',
+            'album': 'Song Album',
+            'art': '/img/a782bb990f9001244de88c9a32b9c14.jpg',
+            'position': 20.4,
+            'length': 213.6
+        }
+    }
+
--- media_player.py
+++ media_player.py
@@ -3,35 +3,76 @@ import dbus
 class MediaPlayer:
     '''Represents a D-Bus connection to a running instance of a supported media player.'''
     
-    # Definitions for common media players.
-    AMAROK = 'org.kde.amarok'
+    # Exception class thrown when a D-Bus error occurs.
+    class Error(Exception):
+        '''Represents a D-Bus error encountered while executing a command.'''
+        
+        def __init__(self, dbus_exception, player):
+            '''Initializes the error with the specified player and error message.'''
+            Exception.__init__(self, dbus_exception.get_dbus_message())
+            self._player = player
+        
+        def get_player(self):
+            '''Returns the name of the player that generated the error.'''
+            return self._player['name']
     
-    def __init__(self, bus_name):
+    # Maintain a connection to the session bus throughout the lifetime of the module.
+    _bus = dbus.SessionBus()
+    
+    # Information about each of the supported media players.
+    _players = {
+        'amarok': {
+            'name': 'Amarok',
+            'bus':  'org.kde.amarok',
+        },
+        'rhythmbox': {
+            'name': 'Rhythmbox',
+            'bus':  'org.gnome.Rhythmbox',
+        },
+    }
+    
+    @classmethod
+    def players(self):
+        '''Returns a list of all media players available.'''
+        return self._players.keys()
+    
+    def __init__(self, player):
         '''Creates a new D-Bus connection to a running instance of the specified media player.'''
-        self._bus = dbus.SessionBus()
-        self._proxy = self._bus.get_object(bus_name, '/Player')
-        self._iface = dbus.Interface(self._proxy, 'org.freedesktop.MediaPlayer')
+        try:
+            self._player = self._players[player]
+            self._proxy = self._bus.get_object(self._player['bus'], '/Player')
+            self._iface = dbus.Interface(self._proxy, 'org.freedesktop.MediaPlayer')
+        except KeyError:
+            raise Exception('media player "%s" is not supported' % player)
+        except dbus.exceptions.DBusException, e:
+            raise self.Error(e, self._player)
     
-    def info(self):
-        '''Returns information about the currently playing song.'''
-        return self._iface.GetMetadata()
+    def _action(self, action, *args):
+        '''Performs the requested action on the player.'''
+        try:
+            self._iface.__getattr__(action)(*args)
+        except dbus.exceptions.DBusException, e:
+            raise self.Error(e, self._player)
+    
+    def _track_changed(self, sender):
+        print 'Track CHANGED!'
     
     def play(self):
         '''Begins playing the current song.'''
-        self._iface.Play()
+        self._action('Play')
     
     def pause(self):
         '''Pauses playback of the current song.'''
-        self._iface.Pause()
+        self._action('Pause')
     
     def stop(self):
         '''Immediately stops playback.'''
-        self._iface.Stop()
+        self._action('Stop')
     
     def prev(self):
         '''Plays the preceding song in the current playlist.'''
-        self._iface.Prev()
+        self._action('Prev')
     
     def next(self):
         '''Plays the next song in the current playlist.'''
-        self._iface.Next()
+        self._action('Next')
